ba5320999893f0ff44253c405a2d55559feca8c1
alsh9887 <alsh9887@OSS327-06.stthomas.edu>
2018-10-04 15:55:57 -0500

    commit 2 . Add skunkApp class

---
+++ SkunkProject/src/SkunkApp.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.introcs.*;
+// Java class skunkApp to print message
+public class SkunkApp {
+	public static void main(String[] args)
+	{
+	
+		
+		StdOut.print("Welcome our simulated program");
+}}
